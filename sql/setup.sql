afd714276c7ca3e7ab0ede6c71baa5e84851cd73
Cody-Wise <cody@sharptackmedia.com>
2022-06-15 17:55:55 -0700

    Setup db, blank testfile, blank controller and blank model files for 1st resource plants

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS plants;
+
+CREATE TABLE plants (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    scientific_name VARCHAR
+);
+
+
+
+
+INSERT INTO plants (name, scientific_name) 
+
+VALUES 
+('Cactus', 'Cactaceae'),
+('Swiss Cheese Plant', 'Monstera deliciosa'),
+('Rubber Plant', 'Ficus elastica')
